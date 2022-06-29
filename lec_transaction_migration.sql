d7bc17618a99c08477c0337f828437e90b437ca9
Chase Martinez <chasemartinez22@gmail.com>
2022-06-29 14:24:58 -0500

    feat: add albums_migration.sql and update to lecture notes

---
+++ lec_transaction_migration.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS transactions;
+
+CREATE TABLE transactions(
+    id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
+    created_at DATETIME NOT NULL,
+    memo VARCHAR(200) DEFAULT 'No memo yet.',
+    account_id INT NOT NULL,
+    is_fraudulent BOOLEAN DEFAULT 0,
+    PRIMARY KEY(id)
+);
