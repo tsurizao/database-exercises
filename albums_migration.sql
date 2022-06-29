db390b4ca9423ab70bc6598c4ad87aa215a66c4d
Chase Martinez <chasemartinez22@gmail.com>
2022-06-29 15:46:28 -0500

    refactor: changed table settings to match lecture

--- albums_migration.sql
+++ albums_migration.sql
@@ -5,10 +5,10 @@ DROP TABLE IF EXISTS albums;
 CREATE TABLE albums
 (
     id           INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist       VARCHAR(50) DEFAULT 'UNKNOWN',
-    name         VARCHAR(50) DEFAULT 'UNKNOWN',
+    artist       VARCHAR(200) DEFAULT 'Unknown' NOT NULL,
+    name         VARCHAR(200) DEFAULT 'Untitled' NOT NULL,
     release_date INT,
     sales        FLOAT,
-    genre        VARCHAR(50) DEFAULT 'UNKNOWN',
+    genre        VARCHAR(100),
     PRIMARY KEY (id)
 );
