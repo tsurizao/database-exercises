f2aaa411fcc3c0345b72826be102da63af29647a
Chase Martinez <chasemartinez22@gmail.com>
2022-07-01 10:09:02 -0500

    Feat: Add exercise files where_exercises.sql, order_by_exercises.sql and limit_exercises.sql Feat: Completed where_exercises.sql

---
+++ limit_exercises.sql
@@ -0,0 +1,8 @@
+# MySQL provides a way to return only unique results from our queries with the keyword DISTINCT.  For example to find all the unique titles within a company, we could run the following query:
+#SELECT DISTINCT title FROM titles;
+
+# List the first 10 distinct last name sorted in descending order.
+
+# Create a query to get the top 5 salaries and display just the employees number from the salaries table.
+
+# Try to think of your results as batches, sets or pages.  The first five results are your first page.  The five after that would be your second page, etc.  Update the previous query to find the tenth page of results
---
+++ order_by_exercises.sql
@@ -0,0 +1,9 @@
+# Modify your fist query to order by first name.  The first result should be Irena Pelz and the last results should be Vidya Awdeh
+
+# Update the query to order by first name and then last name.  The first result should now be Irena Acton and the last should be Vidya Zweizig
+
+# Change the ORDER BY clause so that you order by last name before first name.  Your first result should still be Irena Acton but now the last result should be Maya Zyda.
+
+# Update your queries for employees with 'e' in their last name to sort the results by their employee number.  Make sure the employee numbers are in the correct order
+
+# Now reverse the sort order for both queries and compare the results.
