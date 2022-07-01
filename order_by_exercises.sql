aa1da65bd1446fd0cf3d47da0a0df066acd9cf34
Chase Martinez <chasemartinez22@gmail.com>
2022-07-01 10:41:48 -0500

    Feat: order_by_exercises.sql completed Refactor: where_exercise.sql to remove extra spaces and lines

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,9 +1,18 @@
+USE employees;
+
 # Modify your fist query to order by first name.  The first result should be Irena Pelz and the last results should be Vidya Awdeh
+SELECT * FROM employees WHERE gender ='M' AND first_name = 'Irena' OR gender = 'M' AND first_name = 'Vidya' OR gender = 'M' AND first_name = 'Maya' ORDER BY first_name;
 
 # Update the query to order by first name and then last name.  The first result should now be Irena Acton and the last should be Vidya Zweizig
+SELECT * FROM employees WHERE gender ='M' AND first_name = 'Irena' OR gender = 'M' AND first_name = 'Vidya' OR gender = 'M' AND first_name = 'Maya' ORDER BY first_name, last_name;
 
 # Change the ORDER BY clause so that you order by last name before first name.  Your first result should still be Irena Acton but now the last result should be Maya Zyda.
+SELECT * FROM employees WHERE gender ='M' AND first_name = 'Irena' OR gender = 'M' AND first_name = 'Vidya' OR gender = 'M' AND first_name = 'Maya' ORDER BY last_name, first_name;
 
 # Update your queries for employees with 'e' in their last name to sort the results by their employee number.  Make sure the employee numbers are in the correct order
+SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%e' ORDER BY emp_no;
+SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%e' ORDER BY emp_no;
 
 # Now reverse the sort order for both queries and compare the results.
+SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%e' ORDER BY emp_no DESC;
+SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%e' ORDER BY emp_no DESC;
--- where_exercises.sql
+++ where_exercises.sql
@@ -18,11 +18,10 @@ SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR fi
 SELECT * FROM employees WHERE gender = 'M' AND first_name = 'Irena' OR gender = 'M' AND first_name = 'Vidya' OR gender = 'M' AND first_name = 'Maya';
 
 # Find all employees whose last name starts or ends with 'E' -- 30,723 rows
-SELECT * FROM employees WHERE last_name LIKE 'E%' OR '%e';
+SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%e';
 
-# Duplicate the previous query and update  it to find all employees whose last name starts AND ends with 'E' -- 899 rows
-SELECT * FROM employees WHERE last_name LIKE 'E%' AND '%e';
-
+# Duplicate the previous query and update it to find all employees whose last name starts AND ends with 'E' -- 899 rows
+SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%e';
 
 # Find all employees with a 'q' in their last name but not 'qu' -- 547 rows
 SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
