856ca0e19039825249177b124e93a5c091153cea
Chase Martinez <chasemartinez22@gmail.com>
2022-07-02 13:45:51 -0500

    Updated file name as indexes_practice.sql Feat: all tables described

---
+++ indexes_practice.sql
@@ -0,0 +1,11 @@
+USE employees;
+
+SHOW tables;
+DESCRIBE current_dept_emp; # No indexes or keys
+DESCRIBE departments; # primary key and unique key indexes
+DESCRIBE dept_emp; # two primary key indexes
+DESCRIBE dept_emp_latest_date; # No indexes or keys
+DESCRIBE dept_manager; # two primary key indexes
+DESCRIBE employees; # primary key indexes
+DESCRIBE salaries; # two primary key indexes
+DESCRIBE titles; # three primary key indexes
