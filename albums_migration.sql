64385bcd201393422068273fe659d8a1a3181f52
Chase Martinez <chasemartinez22@gmail.com>
2022-07-02 14:04:44 -0500

    Updated albums_migration.sql to include a unique key index for album name and artist

--- albums_migration.sql
+++ albums_migration.sql
@@ -8,15 +8,17 @@ DESCRIBE albums;
 
 DROP TABLE IF EXISTS albums;
 
-SELECT id, artist, name, release_date, sales, genre FROM albums;
+SELECT id, artist, name, release_date, sales, genre
+FROM albums;
 
 CREATE TABLE albums
 (
     id           INT UNSIGNED AUTO_INCREMENT,
-    artist       VARCHAR(200) DEFAULT 'Unknown' NOT NULL,
+    artist       VARCHAR(200) DEFAULT 'Unknown'  NOT NULL,
     name         VARCHAR(200) DEFAULT 'Untitled' NOT NULL,
     release_date INT,
     sales        FLOAT,
     genre        VARCHAR(100),
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    UNIQUE (name, artist)
 );
--- indexes_practice.sql
+++ indexes_practice.sql
@@ -1,11 +1,20 @@
 USE employees;
 
 SHOW tables;
-DESCRIBE current_dept_emp; # No indexes or keys
+DESCRIBE current_dept_emp; # no key indexes
 DESCRIBE departments; # primary key and unique key indexes
 DESCRIBE dept_emp; # two primary key indexes
-DESCRIBE dept_emp_latest_date; # No indexes or keys
+DESCRIBE dept_emp_latest_date; # no key indexes
 DESCRIBE dept_manager; # two primary key indexes
-DESCRIBE employees; # primary key indexes
+DESCRIBE employees; # primary key index
 DESCRIBE salaries; # two primary key indexes
 DESCRIBE titles; # three primary key indexes
+
+USE codeup_test_db;
+
+SHOW tables;
+DESCRIBE albums; # primary key index
+DESCRIBE books; # primary key index
+DESCRIBE posts; # no key indexes
+DESCRIBE transactions; # primary key index
+DESCRIBE users; # no key indexes
