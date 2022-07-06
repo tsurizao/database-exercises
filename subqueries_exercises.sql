5f92e3fabaea7e773e4e4a913e58c55860a4e1c5
Chase Martinez <chasemartinez22@gmail.com>
2022-07-06 10:50:32 -0500

    Feat: Exercise 3 completion

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -6,6 +6,8 @@ SELECT *
 FROM employees;
 SELECT *
 FROM dept_emp;
+SELECT *
+FROM dept_manager;
 # Find all the employees with the same hire date as employee 101010 using a subquery
 -- 69 rows
 SELECT CONCAT(first_name, ' ', last_name)
@@ -14,8 +16,27 @@ WHERE hire_date = (SELECT hire_date FROM employees WHERE emp_no = 101010);
 
 # Find all the titles held by all employees with the first name Aamod
 -- 314 total titles, 6 unique titles
-SELECT CONCAT(first_name, ' ', last_name) AS full_name, t.title
+SELECT title, CONCAT(e.first_name, ' ', e.last_name) AS full_name
+FROM titles t
+         JOIN employees e ON t.emp_no = e.emp_no
+WHERE t.emp_no IN (SELECT emp_no FROM employees WHERE first_name = 'Aamod');
+
+# Find all the current department managers that are female
+# Results should look like this:
+# +------------+------------+
+# | first_name | last_name  |
+# +------------+------------+
+# | Isamu      | Legleitner |
+# | Karsten    | Sigstam    |
+# | Leon       | DasSarma   |
+# | Hilary     | Kambil     |
+# +------------+------------+
+SELECT first_name, last_name
 FROM employees e
-         JOIN titles t ON e.emp_no = t.emp_no
-WHERE first_name = 'Aamod'
-ORDER BY t.title, last_name;
+WHERE gender = 'F' && e.emp_no IN (SELECT dm.emp_no FROM dept_manager dm WHERE dm.to_date > NOW());
+
+# Easier way to write the above query ...
+# SELECT first_name, last_name
+# FROM employees e
+#          JOIN dept_manager dm ON dm.emp_no = e.emp_no
+# WHERE dm.to_date > NOW() && gender = 'F';
