48143a68066cd2adcce2c8d07662879597f57620
Chase Martinez <chasemartinez22@gmail.com>
2022-07-06 09:46:22 -0500

    Add: Exercise 1 completion

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,1 +1,7 @@
 USE employees;
+
+# Find all the employees with the same hire date as employee 101010 using a subquery
+-- 69 rows
+SELECT CONCAT(first_name, ' ', last_name)
+FROM employees
+WHERE hire_date = (SELECT hire_date FROM employees WHERE emp_no = 101010);
